4eedf62f127c1dc15dbb00b3a714bd9ff420b4ab
Hoa <nxhoaf@gmail.com>
2018-02-03 12:30:57 +0100

    Implement: Add two number should work

--- src/main/java/com/test/calculator/StringCalculator.java
+++ src/main/java/com/test/calculator/StringCalculator.java
@@ -3,6 +3,8 @@ package com.test.calculator;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
+import java.util.stream.Stream;
+
 /**
  * @author nxhoaf
  */
@@ -12,11 +14,18 @@ public class StringCalculator {
             return 0;
         }
         
-
-        if (!NumberUtils.isCreatable(numbers)) {
+        String[] numberStr = numbers.split(",");
+        if (isContainInvalidNumber(numberStr)) {
             throw new IllegalArgumentException("Not a valid number: " + numbers);
         }
+        
+        return Stream.of(numberStr)
+                .map(Integer::valueOf)
+                .reduce(0, (n1, n2) -> n1 + n2);
+    }
 
-        return Integer.valueOf(numbers);
+    private boolean isContainInvalidNumber(String[] numbers) {
+        return Stream.of(numbers)
+                .anyMatch(number -> !NumberUtils.isCreatable(number));
     }
 }
