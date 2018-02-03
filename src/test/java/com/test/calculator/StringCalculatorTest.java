d269b2b89ca4f08e5940f8fdbdaa8806580e4a6f
Hoa <nxhoaf@gmail.com>
2018-02-03 12:15:49 +0100

    Test case: Invalid number should throw an exeption

--- src/test/java/com/test/calculator/StringCalculatorTest.java
+++ src/test/java/com/test/calculator/StringCalculatorTest.java
@@ -1,6 +1,9 @@
 package com.test.calculator;
 
+import com.sun.media.sound.InvalidFormatException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,17 +11,27 @@ import static org.junit.Assert.assertEquals;
  * @author nxhoaf
  */
 public class StringCalculatorTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+    
     private StringCalculator calculator = new StringCalculator();
     
     @Test
-    public void emptyStringShouldReturnZero() {
+    public void addWithEmptyStringShouldReturnZero() {
         int result = calculator.add("");
         assertEquals(0, result);
     }
     
     @Test
-    public void oneNumberShouldReturnItself() {
+    public void addWithOneNumberShouldReturnItself() {
         int result = calculator.add("1");
         assertEquals(1, result);
+    }
+    
+    @Test
+    public void addWithInvalidNumberShouldThrowAnException() {
+        expectedException.expect(InvalidFormatException.class);
+        expectedException.expectMessage("Not a valid numberaaa");
+        calculator.add("invalid");
     }
 }
