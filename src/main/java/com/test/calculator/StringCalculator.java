ac05a24c6d8fcb122dec2751fc9d22fe982be5b8
Hoa <nxhoaf@gmail.com>
2018-02-03 12:04:26 +0100

    Implement: add with empty string should return zero

---
+++ src/main/java/com/test/calculator/StringCalculator.java
@@ -0,0 +1,17 @@
+package com.test.calculator;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author nxhoaf
+ */
+public class StringCalculator {
+    public int add(String numbers) {
+        if (StringUtils.isEmpty(numbers)) {
+            return 0;
+        }
+        
+        
+        return -1;
+    }
+}
